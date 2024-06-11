8f7ab2c88466f4872a74a7bb83a3178eabe540a3
mokrota21 <mokrota.21@gmail.com>
2024-06-11 16:29:46 +0200

    Implemented all states

--- Assignment_1/FMS.py
+++ Assignment_1/FMS.py
@@ -5,25 +5,94 @@ from vi import Agent, Simulation
 from vi.config import Config, dataclass, deserialize
 import random
 
-RADIUS = 200
+COUNT = 100
 
 @deserialize
 @dataclass
 class FMSConfig(Config):
-    speed: float = 15.0
-    p_join: float = 0.8
-    p_leave: float = 0.4
-    t_join: float = RADIUS / speed
-    t_leave: float = RADIUS * 2
-    
-class Cockroach(Agent):
-    config: FMSConfig
-    
-    def change_position(self):
-        self.there_is_no_escape()
+    speed: float = 30.0
+    p_join: float = 1.0
+    p_leave: float = 1 / COUNT
+    D: int = 100
+
+def wander(self):
+        if self.on_site_id() is not None:
+            self.enter_site = Vector2(self.pos.x, self.pos.y)
+            self.escape_site = None
+            self.current = "Join"
+            return
+
         movement = Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize() * self.config.speed
         self.move = movement
         self.pos += self.move
+    
+def join(self):
+        if self.escape_site is not None:
+            self.randomize_site()
+            self.still_i = 0
+            self.current = "Still"
+            return
+
+        self.move = self.move.normalize() * self.config.speed
+        self.pos += self.move
+        self.join_t += 1
+        if not self.on_site():
+            self.escape_site = self.pos - self.move
+
+def still(self):
+    leave = False
+    if self.still_i % self.config.D == 0:
+        leave = random.uniform(0.0, 1.0) < self.leave_prob()
+
+    if leave:
+        self.continue_movement()
+        self.current = "Leave"
+        return
+
+def leave(self):
+    if self.join_t == 0:
+        self.current = "Wander"
+        return
+    
+    self.move = self.move.normalize() * self.config.speed
+    self.pos += self.move
+    self.join_t -= 1
+    
+
+class Cockroach(Agent):
+    config: FMSConfig
+    in_site: bool = False
+    last_site = None
+    current = "Wander"
+    functions = {"Wander": wander, "Join": join, "Still": still, "Leave": leave}
+    join_t = 0
+    still_i = 0
+    enter_site = None
+    escape_site = None
+
+    def leave_prob(self):
+        return self.in_proximity_performance().count() * self.config.p_leave
+
+    def randomize_site(self):
+        pos_v = self.escape_site - self.enter_site
+        rand_l = random.uniform(0, pos_v.length())
+        # rand_l = pos_v.length() / 2
+        if pos_v.length()  != 0:
+            rand_vector = pos_v.normalize() * rand_l
+            # print(rand_vector, self.enter_site)
+            self.pos = self.enter_site + rand_vector
+        self.freeze_movement()
+
+    def change_position(self):
+        self.there_is_no_escape()
+
+        self.functions[self.current](self)
+
+        # self.move = Vector2(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]) - self.pos
+        # self.pos += self.move
+        # print(self.on_site_id())
+
+        
 
 class Selection(Enum):
     SPEED = auto()
@@ -31,43 +100,19 @@ class Selection(Enum):
     SEPARATION = auto()
 
 class FMSLive(Simulation):
-    selection: Selection = Selection.ALIGNMENT
-    config: FMSConfig
+    tmp: int
 
-    def handle_event(self, by: float):
-        if self.selection == Selection.ALIGNMENT:
-            self.config.alignment_weight += by
-        elif self.selection == Selection.COHESION:
-            self.config.cohesion_weight += by
-        elif self.selection == Selection.SEPARATION:
-            self.config.separation_weight += by
-        
-
-    def before_update(self):
-        super().before_update()
-
-        for event in pg.event.get():
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
-                    self.handle_event(by=0.1)
-                elif event.key == pg.K_DOWN:
-                    self.handle_event(by=-0.1)
-                elif event.key == pg.K_1:
-                    self.selection = Selection.ALIGNMENT
-                elif event.key == pg.K_2:
-                    self.selection = Selection.COHESION
-                elif event.key == pg.K_3:
-                    self.selection = Selection.SEPARATION
-
-(
-    FMSLive(
-        FMSConfig(
+config = FMSConfig(
             image_rotation=True,
             movement_speed=1,
-            radius=RADIUS,
+            radius=50,
             seed=1,
         )
+(
+    FMSLive(
+        config
     )
-    .batch_spawn_agents(50, Cockroach, images=["images/green.png"])
+    .spawn_site("images/site1.png", config.window.as_tuple()[0] / 2, config.window.as_tuple()[0] / 2)
+    .batch_spawn_agents(COUNT, Cockroach, images=["images/red.png"])
     .run()
 )
