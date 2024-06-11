dce115619201909b045e06d9e13c1b07457b3207
mokrota21 <mokrota.21@gmail.com>
2024-06-11 18:12:33 +0200

    finished project

--- Assignment_1/FMS.py
+++ Assignment_1/FMS.py
@@ -6,25 +6,33 @@ from vi.config import Config, dataclass, deserialize
 import random
 
 COUNT = 100
+ALL = []
+CAME = 0
+TIME = 0
+INSIDE = [0, 0]
 
 @deserialize
 @dataclass
 class FMSConfig(Config):
     speed: float = 30.0
-    p_join: float = 1.0
-    p_leave: float = 1 / COUNT
+    p_join: float = 0.8
+    p_leave: float = 0.4
     D: int = 100
 
 def wander(self):
-        if self.on_site_id() is not None:
-            self.enter_site = Vector2(self.pos.x, self.pos.y)
-            self.escape_site = None
-            self.current = "Join"
-            return
+    global CAME, INSIDE
+    if self.on_site_id() is not None and random.uniform(0, 1) < self.config.p_join:
+        CAME += 1
+        self.last_site = self.on_site_id()
+        self.enter_site = Vector2(self.pos.x, self.pos.y)
+        self.escape_site = None
+        INSIDE[self.last_site] += 1
+        self.current = "Join"
+        return
 
-        movement = Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize() * self.config.speed
-        self.move = movement
-        self.pos += self.move
+    movement = Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize() * self.config.speed
+    self.move = movement
+    self.pos += self.move
     
 def join(self):
         if self.escape_site is not None:
@@ -40,12 +48,14 @@ def join(self):
             self.escape_site = self.pos - self.move
 
 def still(self):
+    global INSIDE
     leave = False
     if self.still_i % self.config.D == 0:
         leave = random.uniform(0.0, 1.0) < self.leave_prob()
 
     if leave:
         self.continue_movement()
+        INSIDE[self.last_site] -= 1
         self.current = "Leave"
         return
 
@@ -71,7 +81,10 @@ class Cockroach(Agent):
     escape_site = None
 
     def leave_prob(self):
-        return self.in_proximity_performance().count() * self.config.p_leave
+        count = INSIDE[self.last_site]
+        if count == 0:
+            return self.config.p_leave
+        return ((count ** (-2)) * self.config.p_leave)
 
     def randomize_site(self):
         pos_v = self.escape_site - self.enter_site
@@ -84,6 +97,14 @@ class Cockroach(Agent):
         self.freeze_movement()
 
     def change_position(self):
+        global TIME, ALL, CAME
+        if self.id == 0:
+            TIME += 1
+            if TIME == self.config.D:
+                ALL.append([CAME, INSIDE])
+                CAME = 0
+                TIME = 0
+            
         self.there_is_no_escape()
 
         self.functions[self.current](self)
@@ -112,7 +133,9 @@ config = FMSConfig(
     FMSLive(
         config
     )
-    .spawn_site("images/site1.png", config.window.as_tuple()[0] / 2, config.window.as_tuple()[0] / 2)
+    .spawn_site("images/circle2.png", config.window.as_tuple()[0] / 4, config.window.as_tuple()[0] / 4)
+    .spawn_site("images/site1.png", config.window.as_tuple()[0] / 4 * 3, config.window.as_tuple()[0] / 4)
     .batch_spawn_agents(COUNT, Cockroach, images=["images/red.png"])
     .run()
 )
+print(INSIDE)
